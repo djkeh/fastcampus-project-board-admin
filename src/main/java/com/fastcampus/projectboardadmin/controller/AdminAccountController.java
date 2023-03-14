0df64e860bbe21adbb04d84ed1317632f572921b
Uno Kim <djkehh@gmail.com>
2023-03-15 03:10:48 +0900

    #17 - 어드민 회원 컨트롤러에 필요한 핸들러 메소드 및 테스트

--- src/main/java/com/fastcampus/projectboardadmin/controller/AdminAccountController.java
+++ src/main/java/com/fastcampus/projectboardadmin/controller/AdminAccountController.java
@@ -1,23 +1,32 @@
 package com.fastcampus.projectboardadmin.controller;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
+import com.fastcampus.projectboardadmin.dto.response.AdminAccountResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/admin/members")
 @Controller
 public class AdminAccountController {
 
     @GetMapping
-    public String members(
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            Model model
-    ) {
+    public String members(Model model) {
         return "admin/members";
+    }
+
+    @ResponseBody
+    @GetMapping("/api/admin/members")
+    public List<AdminAccountResponse> getMembers() {
+        return List.of();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    @DeleteMapping("/api/admin/members/{userId}")
+    public void delete(@PathVariable String userId) {
     }
 
 }
