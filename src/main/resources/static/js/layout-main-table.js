408e782a2008d901481e307f4115d9951dfe182c
Uno Kim <djkehh@gmail.com>
2023-03-16 03:29:39 +0900

    #26 - AdminLTE 컴포넌트와 javascript를 활용한 Todo List 구현
    AdminLTE 디자인 요소를 활용하여 간단한 기능의 투두 리스트 구현.
    리스트 정보는 백엔드에 저장하지 않고 브라우저에도 저장하지 않으므로
    화면 새로 고치면 모두 날아감.
    아주 간단하게 만들었으며, 그렇기에 업그레이드할 수 있는 요소도 많다.

---
+++ src/main/resources/static/js/layout-main-table.js
@@ -0,0 +1,21 @@
+$(function () {
+    'use strict'
+
+    // Make the dashboard widgets sortable Using jquery UI
+    $('.connectedSortable').sortable({
+        placeholder: 'sort-highlight',
+        connectWith: '.connectedSortable',
+        handle: '.card-header, .nav-tabs',
+        forcePlaceholderSize: true,
+        zIndex: 999999
+    })
+    $('.connectedSortable .card-header').css('cursor', 'move')
+
+    // jQuery UI sortable for the todo list
+    $('.todo-list').sortable({
+        placeholder: 'sort-highlight',
+        handle: '.handle',
+        forcePlaceholderSize: true,
+        zIndex: 999999
+    })
+})
